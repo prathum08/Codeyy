2291c98bc59c6461b14389722eff082f5f81572d
prathum08 <prathumbhangadia08@gmail.com>
2025-10-25 18:04:49 +0530

    c

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const aiRoutes = require('./routes/ai.routes')
+const express = require('express');
+const aiRoutes = require("./routes/ai.routes")
 const cors = require('cors')
 
+const app = express(); // server create
 
-
-const app = express();
-app.use(express.json())
 app.use(cors())
-app.use('/ai' , aiRoutes)
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-module.exports = app;
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+
+app.use('/ai',aiRoutes)
+module.exports = app
